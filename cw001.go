e6e518d903c2247eebbdb63dd8879d9741c5f243
Catfish1210 <chris.laks@gmail.com>
2023-06-28 18:58:58 +0300

    Codewars func(CreatePhoneNumber) cw001

--- cw001.go
+++ cw001.go
@@ -18,7 +18,8 @@ func Codewars001(numbers [10]uint) string {
 	return output
 }
 
-/* Write a function that accepts an array of 10 integers (between 0 and 9),
+/*
+   Write a function that accepts an array of 10 integers (between 0 and 9),
    that returns a string of those numbers in the form of a phone number.
 
    *CreatePhoneNumber([10]uint{1,2,3,4,5,6,7,8,9,0})  // returns "(123) 456-7890"
