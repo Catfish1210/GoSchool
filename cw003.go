0e14f5aaa07d73b4795fcd21002a4e5fa2411913
Catfish1210 <chris.laks@gmail.com>
2023-06-28 18:56:55 +0300

    Codewars func(DigitalRoot) cw003

--- cw003.go
+++ cw003.go
@@ -14,7 +14,8 @@ func Codewars003(n int) int {
 	}
 }
 
-/*Digital root is the recursive sum of all the digits in a number.
+/*
+  Digital root is the recursive sum of all the digits in a number.
   Given n, take the sum of the digits of n.
   If that value has more than one digit, continue reducing in this
   way until a single-digit number is produced. The input will be a non-negative integer.
