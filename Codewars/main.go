4d55aa55079eeb28ccb152eabf515822162197f2
Catfish1210 <chris.laks@gmail.com>
2023-06-30 23:40:47 +0300

    Codewars test dir

--- Codewars/main.go
+++ Codewars/main.go
@@ -6,6 +6,11 @@ import (
 )
 
 func main() {
-	fmt.Println(goschool.Codewars001([10]uint{1, 2, 3, 4, 5, 6, 7, 8, 9, 0}))
+	// fmt.Println(goschool.Codewars001([10]uint{1, 2, 3, 4, 5, 6, 7, 8, 9, 0}))
+	// fmt.Println(goschool.Codewars002([]int{1, 2, 2, 3, 3, 3, 4, 3, 3, 3, 2, 2, 1}))
+	// fmt.Println(goschool.Codewars003(132189))
+	// fmt.Println(goschool.Codewars004(255, 255, 259))
+	// fmt.Println(goschool.Codewars005("MMVIII"))
+	fmt.Println(goschool.Codewars006("xHPjVsLMfnsLx1L0,AUHNY"))
 
 }
