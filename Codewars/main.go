3a37f235dde07d01a60535a51c214ca3932ca7c8
Catfish1210 <chris.laks@gmail.com>
2023-07-03 16:33:51 +0300

    Codewars test dir

--- Codewars/main.go
+++ Codewars/main.go
@@ -11,6 +11,8 @@ func main() {
 	// fmt.Println(goschool.Codewars003(132189))
 	// fmt.Println(goschool.Codewars004(255, 255, 259))
 	// fmt.Println(goschool.Codewars005("MMVIII"))
-	fmt.Println(goschool.Codewars006("xHPjVsLMfnsLx1L0,AUHNY"))
+	// fmt.Println(goschool.Codewars006("xHPjVsLMfnsLx1L0,AUHNY"))
+	// fmt.Println("2149583361 == >", goschool.Codewars007(2149583361))
+	fmt.Println(goschool.Codewars008(3))
 
 }
