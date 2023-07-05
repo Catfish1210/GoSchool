6893ef777eb2d5f7114e364893ddc2c7722e23a4
Chris <56137942+Catfish1210@users.noreply.github.com>
2023-07-05 22:46:20 +0300

    updated test

--- Codewars/main.go
+++ Codewars/main.go
@@ -6,13 +6,13 @@ import (
 )
 
 func main() {
-	// fmt.Println(goschool.Codewars001([10]uint{1, 2, 3, 4, 5, 6, 7, 8, 9, 0}))
-	// fmt.Println(goschool.Codewars002([]int{1, 2, 2, 3, 3, 3, 4, 3, 3, 3, 2, 2, 1}))
-	// fmt.Println(goschool.Codewars003(132189))
-	// fmt.Println(goschool.Codewars004(255, 255, 259))
-	// fmt.Println(goschool.Codewars005("MMVIII"))
-	// fmt.Println(goschool.Codewars006("xHPjVsLMfnsLx1L0,AUHNY"))
-	// fmt.Println("2149583361 == >", goschool.Codewars007(2149583361))
+	fmt.Println(goschool.Codewars001([10]uint{1, 2, 3, 4, 5, 6, 7, 8, 9, 0}))
+	fmt.Println(goschool.Codewars002([]int{1, 2, 2, 3, 3, 3, 4, 3, 3, 3, 2, 2, 1}))
+	fmt.Println(goschool.Codewars003(132189))
+	fmt.Println(goschool.Codewars004(255, 255, 259))
+	fmt.Println(goschool.Codewars005("MMVIII"))
+	fmt.Println(goschool.Codewars006("xHPjVsLMfnsLx1L0,AUHNY"))
+	fmt.Println("2149583361 == >", goschool.Codewars007(2149583361))
 	fmt.Println(goschool.Codewars008(3))
 
 }
